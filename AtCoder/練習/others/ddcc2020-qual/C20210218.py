6790a442d75b5a8dfdd9d79d013dc17c00040e85
hiroyaonoe <onoehiroya@gmail.com>
2021-02-25 00:22:30 +0900

    fix

--- AtCoder/練習/others/ddcc2020-qual/C20210218.py
+++ AtCoder/練習/others/ddcc2020-qual/C20210218.py
@@ -1,60 +1,51 @@
 # editorial
 
-h,w,k = list(map(int,input().split()))
-s=[]
-compressed=[]
-h_num=[]
-idx=-1
-berry=[False for i in range(w)]
+h, w, k = list(map(int, input().split()))
+s = []
+cut_h = [False for i in range(h)]
+first = True
 for i in range(h):
     ss = list(input())
-    ok=False
-    for j in range(w):
-        if ss[j]=='#':
-            berry[j]=True
-            ok=True
-    if ok:
-        compressed.append(berry)
-        berry = [False for i in range(w)]
-        h_num.append(i-idx)
-        idx=i
+    if '#' in ss:
+        if first:
+            first = False
+        else:
+            cut_h[i] = True
+    s.append(ss)
+cut_h[0] = True
 
-        for j in range(w):
-
-
-for berry
-cnt=0
-idx=0
-berry=[False for i in range(w)]
+cnt = 0
+idx = 0
+berry = [False for i in range(w)]
 for i in range(h):
     if cut_h[i]:
-        first=True
-        out_l=[]
+        first = True
+        out_l = []
         for j in range(w):
             if berry[j]:
                 if not first:
                     cnt += 1
-            first=False
+            first = False
             out_l.append(str(cnt))
-        out=" ".join(out_l)
-        for p in range(i-idx):
+        out = " ".join(out_l)
+        for p in range(i - idx):
             print(out)
 
-        idx=i
+        idx = i
         cnt += 1
-        berry=[False for i in range(w)]
+        berry = [False for i in range(w)]
     for j in range(w):
-        if s[i][j]=='#':
-            berry[j]=True
+        if s[i][j] == '#':
+            berry[j] = True
 
-first=True
-out_l=[]
+first = True
+out_l = []
 for j in range(w):
     if berry[j]:
         if not first:
             cnt += 1
-    first=False
+    first = False
     out_l.append(str(cnt))
-out=" ".join(out_l)
-for p in range(h-idx):
+out = " ".join(out_l)
+for p in range(h - idx):
     print(out)
